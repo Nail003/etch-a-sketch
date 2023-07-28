65e7a5ce0869f3a7aecd5acd14bf4cecb1bdb14d
Nail003 <danialzafarstudy003@gmail.com>
2023-07-28 06:49:48 +0500

    [WIP] update gridCreation function to wrap each row in separate subGridContainer div for styling purposes

--- js/javascript.js
+++ js/javascript.js
@@ -8,12 +8,23 @@ function createGrid(size) {
      * size sets the dimensions of the square
      * i.e size = 16 will create a square grid of 16x16
      */
-    const gridElement = document.createElement("div");
-    gridElement.classList.add("gridElement");
+    console.groupCollapsed("%cGrid Creation", "color: green;");
 
-    const area = size * size;
-    for (let i = 0; i < area; i++) {
-        gridContainer.appendChild(gridElement);
-        console.log(`Created grid element No. ${i}`);
+    for (let row = 1; row <= size; row++) {
+        const subGridContainer = document.createElement("div");
+        subGridContainer.classList.add("subGridContainer");
+        gridContainer.appendChild(subGridContainer);
+
+        console.log(`%cCreated row No. ${row}`, "color: lightgreen;");
+
+        for (let column = 1; column <= size; column++) {
+            const gridElement = document.createElement("div");
+            gridElement.classList.add("gridElement");
+            subGridContainer.appendChild(gridElement);
+
+            console.log(`Added grid element No. ${column}`);
+        }
     }
+
+    console.groupEnd();
 }
