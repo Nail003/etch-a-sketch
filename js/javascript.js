209488955dba874d4bb7b39ce35e0ec8c8e1e092
Nail003 <danialzafarstudy003@gmail.com>
2023-07-28 07:58:32 +0500

    [FTR] add form to change gridSize

--- js/javascript.js
+++ js/javascript.js
@@ -1,8 +1,43 @@
 // Loading DOM elements
 const gridContainer = document.querySelector("#gridContainer");
+const form = document.querySelector("form");
+
 createGrid(16);
+logMessageSeparator();
+
+form.addEventListener("submit", submitHandler);
+
+// Event handlers
+function submitHandler(e) {
+    e.preventDefault();
+    let newGridSize = e.target[0].value;
+    if (newGridSize < 1 || newGridSize > 100) {
+        console.log("%cWrong grid input detected", "color: red;");
+        console.log(
+            "%cChanging grid size to default value of 16",
+            "color: orange;"
+        );
+        newGridSize = 16;
+    }
+
+    clearGridContainer();
+    createGrid(newGridSize);
+    console.log(
+        `%cNew grid of size ${newGridSize}x${newGridSize} created`,
+        "color: aqua;"
+    );
+    logMessageSeparator();
+}
 
 // Helper functions
+function clearGridContainer() {
+    /**
+     * Removes all elements from grid container
+     */
+    gridContainer.innerHTML = "";
+    console.log("%cRemoved all children of gridContainer", "color: coral;");
+}
+
 function createGrid(size) {
     /**
      * Create a square grid of "div" elements in gridContainer
@@ -29,4 +64,11 @@ function createGrid(size) {
     }
 
     console.groupEnd();
+}
+
+function logMessageSeparator() {
+    /**
+     * Just a beauty function to separate the console logs
+     */
+    console.log("%c----------Separator----------", "color: darkgrey;");
 }
